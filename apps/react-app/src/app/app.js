0d0eacb375c6483862a300633c0bb62f99076021
Marian <wyz_marian_013@yahoo.com>
2021-05-28 20:40:47 +0300

    add routes to pages and authentication context

--- apps/react-app/src/app/app.js
+++ apps/react-app/src/app/app.js
@@ -1,138 +1,82 @@
-import React from 'react';
-import styles from './app.module.css';
-import { ReactComponent as Logo } from './logo.svg';
-import star from './star.svg';
+import React, { Component, useEffect, useState } from "react";
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import OtherProfile from './pages/OtherProfile';
+import MyProfile from './pages/MyProfile';
+import Generator from './pages/Generator';
+import Settings from './pages/Settings'
+import Layout from './components/Layout';
+import Tree from './pages/Tree';
+import SnowFlake from './pages/SnowFlake';
+import Mountain from './pages/Mountain';
+import ImageCreator from "./pages/ImageCreator";
+import Posting from "./pages/Posting";
+import VerifyCode from "./pages/VerifyCode";
+import ChangePassword from "./pages/ChangePassword";
+import ResetPassword from "./pages/ResetPassword";
+import EmailForReset from "./pages/EmailForReset";
 
-import { Route, Link } from 'react-router-dom';
-export function App() {
+import AuthenticationContext from "./AuthenticationContext";
+import httpService from './services/httpService';
+import { v4 as uuidv4 } from 'uuid';
+
+export default function App(props){
+  const [userId, setUserId] = useState(-1);
+  function logIn(newState) {
+    setUserId(newState);
+    //console.log(newState);
+    localStorage.setItem(
+      "user",
+      JSON.stringify({
+        id: newState,
+      })
+    );
+  }
+
+  function logOut() {
+    setUserId(-1);
+    localStorage.removeItem("user");
+  }
+
+  function getUser(){
+    const user = localStorage.getItem("user");
+    let id = -1;
+    if(user != null){
+      id = JSON.parse(user).id;
+    }
+    setUserId(id);
+    return id;
+  }
+
+  useEffect(()=>{
+    getUser();
+  },[])
+
+
   return (
-    <div className={styles.app}>
-      <header className="flex">
-        <Logo width="75" height="75" />
-        <h1>Welcome to react-app!</h1>
-      </header>
-      <main>
-        <h2>Resources &amp; Tools</h2>
-        <p>Thank you for using and showing some ♥ for Nx.</p>
-        <div className="flex github-star-container">
-          <a
-            href="https://github.com/nrwl/nx"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
-            If you like Nx, please give it a star:
-            <div className="github-star-badge">
-              <img src={star} className="material-icons" alt="" />
-              Star
-            </div>
-          </a>
-        </div>
-        <p>Here are some links to help you get started.</p>
-        <ul className="resources">
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://egghead.io/playlists/scale-react-development-with-nx-4038"
-            >
-              Scale React Development with Nx (Course)
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/latest/react/tutorial/01-create-application"
-            >
-              Interactive tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a className="resource flex" href="https://nx.app/">
-              <svg
-                width="36"
-                height="36"
-                viewBox="0 0 120 120"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M120 15V30C103.44 30 90 43.44 90 60C90 76.56 76.56 90 60 90C43.44 90 30 103.44 30 120H15C6.72 120 0 113.28 0 105V15C0 6.72 6.72 0 15 0H105C113.28 0 120 6.72 120 15Z"
-                  fill="#0E2039"
-                />
-                <path
-                  d="M120 30V105C120 113.28 113.28 120 105 120H30C30 103.44 43.44 90 60 90C76.56 90 90 76.56 90 60C90 43.44 103.44 30 120 30Z"
-                  fill="white"
-                />
-              </svg>
-              <span className="gutter-left">Nx Cloud</span>
-            </a>
-          </li>
-        </ul>
-        <h2>Next Steps</h2>
-        <p>Here are some things you can do with Nx.</p>
-        <details open>
-          <summary>Add UI library</summary>
-          <pre>{`# Generate UI lib
-nx g @nrwl/react:lib ui
+    <Router>
+      <Switch>
+        <AuthenticationContext.Provider value={{id:userId, logIn:logIn, logOut:logOut, getUser:getUser}}>
+          <Layout>
+            <Route exact path="/" render={(props) => <Home {...props} key={uuidv4()}/>} />
+            <Route path="/verify_code/:code" component={VerifyCode} />
+            <Route path="/reset_password/:code" component={ResetPassword} />
+            <Route path="/change_password" component={ChangePassword} />
+            <Route path="/forgot_password" component={EmailForReset} />
+            <Route path="/myprofile" component={MyProfile} />
+            <Route path="/profile/:id" render={(props) => <OtherProfile {...props} key={uuidv4()}/>} />
+            <Route path="/generator" component={Generator} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/posting/:id" component={Posting} />
+            <Route path="/Tree/:action/:id?" component={Tree} />
+            <Route path="/SnowFlake/:action/:id?" component={SnowFlake} />
+            <Route path="/Mountain/:action/:id?" component={Mountain} />
+            <Route path="/ImageCreator/:action/:id?" component={ImageCreator} />
+          </Layout>
+        </AuthenticationContext.Provider>
+      </Switch>
+    </Router>
 
-# Add a component
-nx g @nrwl/react:component xyz --project ui`}</pre>
-        </details>
-        <details>
-          <summary>View dependency graph</summary>
-          <pre>{`nx dep-graph`}</pre>
-        </details>
-        <details>
-          <summary>Run affected commands</summary>
-          <pre>{`# see what's been affected by changes
-nx affected:dep-graph
-
-# run tests for current changes
-nx affected:test
-
-# run e2e tests for current changes
-nx affected:e2e
-  `}</pre>
-        </details>
-      </main>
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Route
-        path="/"
-        exact
-        render={() => (
-          <div>
-            This is the generated root route.{' '}
-            <Link to="/page-2">Click here for page 2.</Link>
-          </div>
-        )}
-      />
-      <Route
-        path="/page-2"
-        exact
-        render={() => (
-          <div>
-            <Link to="/">Click here to go back to root page.</Link>
-          </div>
-        )}
-      />
-      {/* END: routes */}
-    </div>
   );
 }
-export default App;
